10e88974ff5ea816d86b2e39531e15d76bce655e
Kshitiz83 <149290842+Kshitiz83@users.noreply.github.com>
2023-10-29 11:30:36 -0700

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,7 @@
+adjective=input("Please enter the adjective")
+noun=input("Please enter the noun")
+adverb=input("Please enter the adverb")
+verb=input("Please enter the verb")
+
+madlibs= f"Once upon a time, there was a {adjective} {noun} who loved to {adverb} {verb}. The {noun} and the {verb} were the best of friends, and they spent their days {verb} {adverb}. Everyone in the {adjective} village admired their {adverb} {verb}. The end."
+print(madlibs)
